48fcd4577e1c14989903bffb8e3acfa8570b4eef
Danis96 <danis.preldzic@abc-techgroup.com>
2021-01-04 10:42:27 +0100

    bottom navigation bar

--- lib/app/view/home/page/home_page.dart
+++ lib/app/view/home/page/home_page.dart
@@ -1,15 +1,61 @@
 import 'package:flutter/material.dart';
+import 'package:wallet/app/utils/color_helper.dart';
 import 'package:wallet/app/utils/size_config.dart';
 import 'package:wallet/app/view/home/widgets/home_top_background_container.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int _selectedIndex = 0;
+
+  static const List<Widget> _widgetOptions = <Widget>[
+    Text(
+      'Index 0: Home',
+    ),
+    Text(
+      'Index 1: Business',
+    ),
+    Text(
+      'Index 2: School',
+    ),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     SizeConfig().init(context);
     return Scaffold(
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.credit_card),
+            label: 'Card',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.money),
+            label: 'Transactions',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.account_box),
+            label: 'Profile',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: ColorHelper.walletBlue.color,
+        onTap: _onItemTapped,
+      ),
       body: ListView(
         children: <Widget>[
           topBackgroundContainer(),
+          _widgetOptions.elementAt(_selectedIndex),
         ],
       ),
     );
